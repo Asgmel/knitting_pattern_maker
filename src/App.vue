304a7d38d6e184585f57e71091145ef24724d406
Asgeir Melling <asgmel@gmail.com>
2024-11-06 22:01:07 +0100

    feat: add number input

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,25 @@
   <FileInput button-text="Input File Path" @update:file-path="(filePath: string) => { inputFilePath = filePath }" />
   <FileInput input-type-folder button-text="Output Folder Path"
     @update:file-path="(filePath: string) => { outputFolderPath = filePath }" />
+  <NumberInput label-text="Pattern Scale" v-model="patternScale" />
+  <NumberInput label-text="Border Size" v-model="borderSize" />
   <button @click="submit">Submit</button>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
 import FileInput from './components/FileInput.vue';
+import NumberInput from './components/NumberInput.vue';
 
 let inputFilePath = ref("")
 let outputFolderPath = ref("")
+let patternScale = ref(20)
+let borderSize = ref(2)
 
 const submit = () => {
   console.log(inputFilePath.value)
   console.log(outputFolderPath.value)
+  console.log(patternScale.value)
+  console.log(borderSize.value)
 }
 </script>
---
+++ src/components/NumberInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <label for="numberInput">{{ labelText }}</label>
+    <input id="numberInput" v-model="inputValueModel" type="number">
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  labelText: {
+    type: String,
+    required: true,
+  },
+})
+
+const inputValueModel = defineModel()
+
+console.log(inputValueModel.value)
+</script>
+
+<style scoped></style>
