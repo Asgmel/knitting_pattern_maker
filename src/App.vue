d7f6cd8f0fc645611cf72a5b19b55e62f54f476c
Asgeir Melling <asgmel@gmail.com>
2024-11-17 21:33:07 +0100

    feat: add logic for hex input fields.

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@
       <NumberInput label-text="Pattern Scale" v-model="patternScale" />
       <NumberInput label-text="Border Size" v-model="borderSize" />
     </div>
-    <ColorReplacement v-model:from="fromInput" v-model:to="toInput" />
-    <ColorReplacement />
-    <ColorReplacement />
+    <ColorReplacement v-model:from="fromInput1" v-model:to="toInput2" />
+    <ColorReplacement v-model:from="fromInput2" v-model:to="toInput2" />
+    <ColorReplacement v-model:from="fromInput3" v-model:to="toInput3" />
     <button @click="submit">Submit</button>
   </div>
 </template>
@@ -24,16 +24,24 @@ let inputFilePath = ref("")
 let outputFolderPath = ref("")
 let patternScale = ref(20)
 let borderSize = ref(2)
-let fromInput = ref("abcabc")
-let toInput = ref("#121212")
+let fromInput1 = ref("#ffffff")
+let toInput1 = ref("#ffffff")
+let fromInput2 = ref("#ffffff")
+let toInput2 = ref("#ffffff")
+let fromInput3 = ref("#ffffff")
+let toInput3 = ref("#ffffff")
 
 const submit = () => {
   console.log(inputFilePath.value)
   console.log(outputFolderPath.value)
   console.log(patternScale.value)
   console.log(borderSize.value)
-  console.log(fromInput.value)
-  console.log(toInput.value)
+  console.log(fromInput1.value)
+  console.log(toInput1.value)
+  console.log(fromInput2.value)
+  console.log(toInput2.value)
+  console.log(fromInput3.value)
+  console.log(toInput3.value)
 }
 </script>
 
--- src/components/ColorReplacement.vue
+++ src/components/ColorReplacement.vue
@@ -1,47 +1,71 @@
 <template>
   <div class="container">
-    <input type="text" :value="fromInputValue" @beforeinput="(e: Event) => { fromInputValue = sanitizeHexInput(e) }">
-    <div class="colorBox" :style="{ backgroundColor: validateHexColor(fromInputValue) }"></div>
+    <input type="text" ref="fromInput" :value="fromInputValue"
+      @beforeinput="(e: Event) => { fromInputValue = sanitizeHexInput(e) }"
+      :style="{ border: validateHexColor(fromInputValue) ? 'none' : '1px solid #ff0033' }">
+    <div class="colorBox" :style="{ backgroundColor: validateHexColor(fromInputValue) ? fromInputValue : '#ffffff' }">
+    </div>
     <div class="dash">-</div>
-    <div class="colorBox" :style="{ backgroundColor: validateHexColor(toInputValue) }"></div>
-    <input type="text" :value="toInputValue" @beforeinput="(e: Event) => { toInputValue = sanitizeHexInput(e) }">
+    <div class="colorBox" :style="{ backgroundColor: validateHexColor(toInputValue) ? toInputValue : '#ffffff' }"></div>
+    <input type="text" ref="toInput" :value="toInputValue"
+      @beforeinput="(e: Event) => { toInputValue = sanitizeHexInput(e) }"
+      :style="{ border: validateHexColor(toInputValue) ? 'none' : '1px solid #ff0033' }">
     <button class="change">⇓</button>
 
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { nextTick, ref, Ref, useTemplateRef } from 'vue';
 
 const fromInputValue = defineModel("from")
 const toInputValue = defineModel("to")
+const fromInputRef: Ref<HTMLInputElement> = useTemplateRef("fromInput")
+const toInputRef: Ref<HTMLInputElement> = useTemplateRef("toInput")
+
 
 function validateHexColor(hexColorValue: string) {
-  if (/^#[a-fA-F0-9]{6}$/.test(hexColorValue)) {
-    return hexColorValue;
-  }
-
-  return "#FFFFFF";
+  return /^#[a-fA-F0-9]{6}$/.test(hexColorValue)
 }
 
 const sanitizeHexInput = (event: InputEvent) => {
+  event.preventDefault()
+
   let value = (event.target as HTMLInputElement).value;
-  let input = event.data;
+  const input = event.data;
+  // Do math.max to make sure we never delete the # at space 0
+  const selectionStart = Math.max((event.target as HTMLInputElement).selectionStart, 1)
+  const selectionEnd = Math.max((event.target as HTMLInputElement).selectionEnd, 1)
+  const selectionActive = selectionStart !== selectionEnd
+  let cursorPosition = selectionStart
 
-  if (value.length > 6 && event.inputType === "insertText") {
-    event.preventDefault()
+  if (event.inputType === "insertText") {
+    if ((value.length <= 6 || selectionActive) && /^[a-fA-F0-9]$/.test(input)) { // Only allow hex characters
+      value = `#${value.slice(1, selectionStart) + input + value.slice(selectionEnd, value.length)}`
+      cursorPosition++
+    }
   }
 
-  if (value === "#" && event.inputType === "deleteContentBackward") {
-    event.preventDefault()
+  if (event.inputType === "deleteContentBackward") {
+    if (!selectionActive) {
+      value = `#${value.slice(1, Math.max(selectionStart - 1, 1)) + value.slice(selectionEnd, value.length)}`
+    } else {
+      value = `#${value.slice(1, selectionStart) + value.slice(selectionEnd, value.length)}`
+    }
+    if (cursorPosition != 1) {
+      cursorPosition--
+    }
   }
 
-  if (!/^[a-fA-F0-9]$/.test(input) && event.inputType === "insertText") {
-    event.preventDefault();
+  if (event.inputType === "deleteContentForward") {
+    if (!selectionActive) {
+      value = `#${value.slice(1, selectionStart) + value.slice(Math.min(selectionEnd + 1, value.length - 1), value.length)}`
+    } else {
+      value = `#${value.slice(1, selectionStart) + value.slice(selectionEnd, value.length)}`
+    }
   }
 
   if (event.inputType == "insertFromPaste") {
-    event.preventDefault()
     if (input.startsWith('#')) {
       value = input.slice(0, Math.min(input.length, 7))
     } else {
@@ -55,6 +79,11 @@ const sanitizeHexInput = (event: InputEvent) => {
 
   value = value.replace(/[^#a-fA-F0-9]/g, '');
 
+  setTimeout(() => {
+    fromInputRef.value.focus()
+    fromInputRef.value.setSelectionRange(cursorPosition, cursorPosition);
+  })
+
   return value
 }
 </script>
@@ -63,6 +92,10 @@ const sanitizeHexInput = (event: InputEvent) => {
 .container {
   display: flex;
   gap: 20px;
+}
+
+.dash {
+  color: #ffffff;
 }
 
 .colorBox {
