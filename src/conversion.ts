cccf20ef6309ee12532739ca397eb999bf6ef11d
Asgeir Melling <asgmel@gmail.com>
2025-01-01 16:37:04 +0100

    refactor: refactor the way color replacement works

--- src/conversion.ts
+++ src/conversion.ts
@@ -1,7 +1,9 @@
-import { ConverterSettings } from "./converter.types"
+import { ConverterSettings, ColorReplacement } from "./converter.types"
 import { Jimp } from "jimp";
 
-export const runConversion = async (converterSettings: ConverterSettings) => {
+type JimpType = InstanceType<typeof Jimp>;
+
+export async function runConversion(converterSettings: ConverterSettings) {
   try {
     const originalImage = await Jimp.read(converterSettings.inputFilePath);
 
@@ -11,47 +13,106 @@ export const runConversion = async (converterSettings: ConverterSettings) => {
     const newWidth = originalWidth * converterSettings.patternScale + (originalWidth - 1) * converterSettings.borderSize;
     const newHeight = originalHeight * converterSettings.patternScale + (originalHeight - 1) * converterSettings.borderSize;
 
-    const newImage = new Jimp({ width: newWidth, height: newHeight, color: 0x000000FE });
-
-    const tempcolors = new Set([])
+    let newImage = new Jimp({ width: newWidth, height: newHeight, color: 0x000000FE });
 
     // process each pixel in the original image
     for (let y = 0; y < originalHeight; y++) {
-      pixelLoop:
       for (let x = 0; x < originalWidth; x++) {
-        const originalcolor = originalImage.getPixelColor(x, y);
-        tempcolors.add(`#${originalcolor.toString(16).padStart(6, '0')}`)
+        const originalColor = originalImage.getPixelColor(x, y).toString();
+        console.log(originalColor)
+        const anchorX = x * (converterSettings.patternScale + converterSettings.borderSize);
+        const anchorY = y * (converterSettings.patternScale + converterSettings.borderSize);
 
-        const destX = x * (converterSettings.patternScale + converterSettings.borderSize);
-        const destY = y * (converterSettings.patternScale + converterSettings.borderSize);
 
-        const colorsToReplace = converterSettings.colorReplacements.map((colorReplacement) => colorReplacement.from.toUpperCase())
-
-        // Replace pixels with signs and other colors based on settings
-        for (let color of colorsToReplace) {
-          if (color.toLowerCase() === `#${originalcolor.toString(16).slice(0, 6).toLowerCase()}`) {
-            for (let dy = 0; dy < converterSettings.patternScale; dy++) {
-              for (let dx = 0; dx < converterSettings.patternScale; dx++) {
-                newImage.setPixelColor(0x000000FF, destX + dx, destY + dy)
-              }
-            }
-            continue pixelLoop
+        for (let replacement of converterSettings.colorReplacements) {
+          if (originalColor === replacement.from && replacement.to === "circle") {
+            newImage = transform_pixel_circle(newImage, anchorX, anchorY, converterSettings.patternScale, converterSettings.iconSize, replacement)
           }
-        }
-
-        // default scale up pixels if they are not replaced
-        for (let dy = 0; dy < converterSettings.patternScale; dy++) {
-          for (let dx = 0; dx < converterSettings.patternScale; dx++) {
-            newImage.setPixelColor(originalcolor, destX + dx, destY + dy);
+          else if (originalColor === replacement.from && replacement.to === "dot") {
+            newImage = transform_pixel_dot(newImage, anchorX, anchorY, converterSettings.patternScale, converterSettings.iconSize, replacement)
+          }
+          else if (originalColor === replacement.from && replacement.to === "cross") {
+            newImage = transform_pixel_cross(newImage, anchorX, anchorY, converterSettings.patternScale, converterSettings.iconSize, replacement)
+          }
+          else if (originalColor === replacement.from && replacement.to === "line") {
+            newImage = transform_pixel_line(newImage, anchorX, anchorY, converterSettings.patternScale, converterSettings.iconSize, replacement)
+          }
+          else if (originalColor === replacement.from && replacement.to === "hex") {
+            newImage = transform_pixel_color(newImage, anchorX, anchorY, converterSettings.patternScale, replacement)
+          }
+          else {
+            newImage = transform_pixel_default(newImage, anchorX, anchorY, converterSettings.patternScale)
           }
         }
       }
     }
-    console.log("Colors: ", tempcolors)
     // Save the new image
     await newImage.write(`${converterSettings.outputFolderPath}/processedImage.png`);
     return `Image processed and saved to ${converterSettings.outputFolderPath}/processedImage.png`
   } catch (err) {
     return `Error processing image: ${err}`
   }
+}
+
+function transform_pixel_circle(
+  image: JimpType,
+  anchorX: number,
+  anchorY: number,
+  patternScale: number,
+  iconSize: number,
+  colorReplacement: ColorReplacement
+): JimpType {
+  return image
+}
+
+function transform_pixel_dot(
+  image: JimpType,
+  anchorX: number,
+  anchorY: number,
+  patternScale: number,
+  iconSize: number,
+  colorReplacement: ColorReplacement
+): JimpType {
+  return image
+}
+
+function transform_pixel_cross(
+  image: JimpType,
+  anchorX: number,
+  anchorY: number,
+  patternScale: number,
+  iconSize: number,
+  colorReplacement: ColorReplacement
+): JimpType {
+  return image
+}
+
+function transform_pixel_line(
+  image: JimpType,
+  anchorX: number,
+  anchorY: number,
+  patternScale: number,
+  iconSize: number,
+  colorReplacement: ColorReplacement
+): JimpType {
+  return image
+}
+
+function transform_pixel_color(
+  image: JimpType,
+  anchorX: number,
+  anchorY: number,
+  patternScale: number,
+  colorReplacement: ColorReplacement
+): JimpType {
+  return image
+}
+
+function transform_pixel_default(
+  image: JimpType,
+  anchorX: number,
+  anchorY: number,
+  patternScale: number,
+): JimpType {
+  return image
 }
